cf9999dee5b7e4aabe715544a9a5f62291843d7f
Muna Dahir <muna.dahir@hotmail.com>
2021-11-04 16:54:13 -0400

    made test for breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,16 +3,21 @@ const command = process.argv.slice(2);
 const breed = command[0];
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body); 
+
+    if (body.length === 2 || response && response.statusCode >= 300 || breedName !== data[0].name) {
+      // console.log('Looks like we\'ve got a problem! Please try again with a valid URL/Breed');
+      callback('Looks like we\'ve got a problem! Please try again with a valid URL/Breed', null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+};
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  
-  if (body.length === 2 || response && response.statusCode >= 300) {
-    console.log('Looks like we\'ve got a problem! Please try again with a valid URL/Breed'); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+
+
+module.exports = { fetchBreedDescription };
